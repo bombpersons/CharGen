06fad3c5de906284024c7b1bfaa0dc0126afc316
dtConfect <dtconfect@gmail.com>
2017-02-07 22:33:07 +0000

    Merge fix BaseCharacter->BaseTemplate

--- guiTest.py
+++ guiTest.py
@@ -2,12 +2,12 @@ import GUI.Application as guiApp
 import GUI.tkGUI.Window as tkWindow
 import CharGen.Rulesets.Pathfinder.Rules as pfRules
 
-import CharGen.Rulesets.Pathfinder.BaseCharacter as BaseCharacter
+import CharGen.Rulesets.Pathfinder.BaseTemplate as BaseTemplate
 import CharGen.Rulesets.Pathfinder.Races.Goblin as Goblin
 import CharGen.Rulesets.Pathfinder.Classes.Warrior as Warrior
 
 if __name__ == "__main__":
     app = guiApp.Application(tkWindow.CharacterGUI, pfRules.PathfinderCharacter)
-    app.character.apply(BaseCharacter.BaseCharacter("Bob"))
+    app.character.apply(BaseTemplate.BaseTemplate("Bob"))
     app.character.apply(Warrior.Warrior(10))
     app.run()
