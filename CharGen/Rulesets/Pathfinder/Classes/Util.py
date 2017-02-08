14722c87701a6804671432ec8f1ef668620cac52
bombpersons <blindrabbits@gmail.com>
2017-02-08 17:30:15 +0000

    Adding a random class to a character uses a slightly better method for picking the class based on the stats.

--- CharGen/Rulesets/Pathfinder/Classes/Util.py
+++ CharGen/Rulesets/Pathfinder/Classes/Util.py
@@ -6,24 +6,34 @@ import CharGen.Rulesets.Pathfinder.Classes.Commoner as Commoner
 import random
 
 def GetRandomAppropriateClass(character, lvl):
-    # Classes, with a list of stats that are useful for them.
+    # Classes, with a list of minimum stats
     possibleClasses = {
         #Sorcerer.RandomSorcerer : ["cha"],
-        Wizard.WizardRandom : ["int"],
-        #Warrior.RandomWarrior : ["str"]
+        Wizard.WizardRandom : [0, 0, 0, 11, 0, 0],
+        Warrior.RandomWarrior : [12, 10, 10, 0, 0, 0]
     }
 
     commonerClasses = [
         Commoner.Commoner
     ]
 
+    # Get the character stats
+    charStats = [
+        character.getStat("str"),
+        character.getStat("dex"),
+        character.getStat("con"),
+        character.getStat("int"),
+        character.getStat("wis"),
+        character.getStat("cha")
+    ]
+
     # Figure out the best class for them.
     shortList = []
     for c, stats in possibleClasses.items():
-        qualify = False
-        for stat in stats:
-            if character.getStat(stat) > 14:
-                qualify = True
+        qualify = True
+        for charStat, stat in list(zip(charStats, stats)):
+            if charStat < stat:
+                qualify = False
 
         if qualify:
             shortList += [c]
