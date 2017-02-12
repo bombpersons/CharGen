e483f7e413953e9794d44a020b07abcec0828422
dtConfect <dtconfect@gmail.com>
2017-02-12 19:30:15 +0000

    Enhanced experimental warcry gen

--- warcryGen.py
+++ warcryGen.py
@@ -48,7 +48,28 @@ nouns = [
     noun("Slug", "Slugs"),
     noun("Bargain", "Bargains"),
     noun("Dragon", "Dragons"),
-    noun("Romance", "Romances")
+    noun("Romance", "Romances"),
+    noun("Dog", "Dogs"),
+    noun("Pack", "Packs"),
+    noun("Finger", "Fingers"),
+    noun("Jelly", "Jellies"),
+    noun("Clock", "Clocks"),
+    noun("Freak", "Freaks"),
+    noun("Priest", "Priests"),
+    noun("Insult", "Insults"),
+    noun("Dissapointment", "Dissapointments"),
+    noun("Harvest", "Harvests"),
+    noun("Star", "Stars"),
+    noun("Child", "Children"),
+    noun("Velociraptor", "Velociraptors"),
+    noun("Murder", "Murders"),
+    noun("Killer", "Killers"),
+    noun("Band", "Bands"),
+    noun("Fang", "Fangs"),
+    noun("Captor", "Captors"),
+    noun("Soldier", "Soldiers"),
+    noun("Word", "Words"),
+    noun("World", "Worlds")
 ]
 
 class randomNoun():
@@ -81,12 +102,37 @@ verbs = [
     verb("Fly", "Flying", "Flew"),
     verb("Kill", "Killing", "Killed"),
     verb("Fool", "Fooling", "Fooled"),
-    verb("Tarnish", "Tarnishing", "Tarnished")
+    verb("Tarnish", "Tarnishing", "Tarnished"),
+	verb("Eat", "Eating", "Eaten"),
+	verb("Love", "Loving", "Loved"),
+	verb("Hate", "Hating", "Hate"),
+    verb("Fly", "Flying", "Flew"),
+    verb("Abandon", "Abandoning", "Abandoned"),
+    verb("Grow", "Growing", "Grown"),
+    verb("Shrink", "Shrinking", "Shrunken"),
+    verb("Rotate", "Rotating", "Rotated"),
+    verb("Pickle", "Pickling", "Pickled"),
+    verb("Ferment", "Fermenting", "Fermented"),
+    verb("Sheath", "Sheathing", "Sheathed"),
+    verb("Work", "Working", "Worked"),
+    verb("Capture", "Capturing", "Captured"),
+    verb("Embarass", "Embarassing", "Embarassed"),
+    verb("Twist", "Twisting", "Twisted")
 ]
 
 class randomVerb():
+    def __init__(self,force=None):
+        self.force=force
+
     def __str__(self):
-        return random.choice(verbs).infinitive
+        choices = random.choice(verbs)
+
+        if self.force=="present":
+            return choices.present
+        if self.force=="infinitive":
+            return choices.infinitive
+        else:
+            return random.choice([choices.infinitive, choices.past])
 
 adjectives = [
     "Runty",
@@ -150,12 +196,40 @@ adjectives = [
     "Unkind",
     "Simple",
     "Vengeful",
-    "Cromulant"
+    "Cromulant",
+	"Fighty",
+    "Fishy",
+    "Turbulent",
+    "Hot",
+    "Cold",
+    "Lukewarm",
+    "Ticklish",
+    "Hungry",
+    "Wicked",
+    "Elder",
+    "Old",
+    "Evil",
+    "Holy",
+    "Bottom",
+    "Red",
+    "Crimson",
+    "Yellow",
+    "Blue",
+    "Black",
+    "White",
+    "Dangerous",
+    "Carion",
+    "Slavering",
+    "Scaly"
 ]
 
 class randomAdjective():
     def __str__(self):
         return random.choice(adjectives)
+
+class randomVerbAdjectiveOrNoun():
+    def __str__(self):
+        return str(random.choice([randomVerb(force="past"), randomNoun(force="plural"), randomAdjective()]))
 
 interogatives = [
     "Why",
@@ -167,8 +241,6 @@ interogatives = [
     "Wence",
     "Wherefor",
     "Whither",
-    "Invariably",
-    "Occasionally"
 ]
 
 class randomInterogative():
@@ -182,7 +254,9 @@ qualifiers = [
     "Invariably",
     "Occasionally",
     "Our",
-    "Your"
+    "Your",
+    "Invariably",
+    "Occasionally"
 ]
 
 class randomQualifier():
@@ -193,7 +267,8 @@ questionForms = [
     [randomInterogative(), randomAdjective(), randomNoun()],
     [randomInterogative(), randomNoun()],
     [randomInterogative(), randomVerb()],
-    [randomInterogative(), "not"]
+    [randomInterogative(), "not"],
+    ["What do we want"]
 ]
 
 def getRandomQuestionForm():
@@ -206,7 +281,7 @@ class randomQuestion():
 
 answerForms = [
     ["Because", randomNoun(), "are", randomAdjective()],
-    ["Because", randomQualifier(), randomNoun(), "are", randomAdjective()],
+    ["Because", randomQualifier(), randomNoun(force="plural"), "are", randomAdjective()],
     [randomNoun(), "are", randomNoun()],
     [randomVerb(), randomNoun()]
 ]
@@ -220,28 +295,43 @@ class randomAnswer():
         return resolveForm(answerForm)
 
 factionNameForms = [
-    ["(The)", randomAdjective(), randomNoun()],
-    ["(The)", randomNoun(force="singular"), randomNoun()],
+    ["(The)", randomAdjective(), randomNoun(force="plural")],
+    ["(The)", randomNoun(force="singular"), randomNoun(force="plural")],
     ["(The)", randomNoun(), "of", randomNoun(force="plural")],
+    ["(The)", randomNoun(), "of", randomVerb(force="infinitive")],
     ["(The)", randomAdjective(), randomAdjective(), randomNoun()],
-    ["(The)", randomQualifier(), randomAdjective(), randomNoun()]
+    ["(The)", randomQualifier(), randomAdjective(), randomNoun()],
+    ["(The)", randomQualifier(), randomNoun()]
 ]
 
+class randomSubject():
+    def __str__(self):
+        return str(random.choice([randomNoun(force="plural"), str(randomQualifier()) + " " + str(randomNoun())]))
+
 factionSloganForms = [
+    ["For", randomNoun()],
     [randomQuestion(), "?", randomQuestion(), "?"],
     [randomQuestion(), "?", randomAnswer()],
+    [randomQuestion(), "?", randomNoun(), "!"],
     ["We may be", randomAdjective(), "but we're", randomAdjective(), randomNoun(force="plural")],
     [randomVerb(), randomNoun()],
     ["It's", randomQualifier(), randomNoun()],
     ["It's", randomNoun(), "- not", randomNoun()],
     [randomVerb(), "it"],
+    [randomNoun(), "!", randomNoun(), "!", randomNoun(), "!"],
     ["We're", randomAdjective(), randomNoun(force="plural")],
     ["We're", randomQualifier(), randomNoun(force="plural")],
     ["We're", randomQualifier(), randomAdjective()],
     ["Anything but", randomNoun(), "!"],
     ["Anything but", randomAdjective(), "!"],
-    ["Will", randomVerb(), "for", randomNoun()],
-    ["Hey", randomNoun(), "-", randomVerb(), "a", randomNoun()]
+    ["Will", randomVerb(force="present"), "for", randomNoun()],
+    ["Hey", randomNoun(), "-", randomVerb(), "a", randomNoun(force="singular")],
+    ["We're", randomVerb(force="infinitive"), randomQualifier(), randomNoun()],
+    [randomQualifier(), randomNoun(), randomQualifier(), randomNoun()],
+    ["It's", randomAdjective(), "to go alone - here, take", randomNoun(force="plural")],
+    ["It's", randomQualifier(), randomAdjective(), randomVerb(force="infinitive"), randomVerbAdjectiveOrNoun()],
+    ["You're", randomVerb(force="infinitive"), "my", randomNoun(), ", man."],
+    ["We're gonna", randomVerb(force="present"), randomSubject()]
 ]
 
 def getNoun():
